f0dd6dca36fcf4e31bf545d6ef6a3e4f817d9c37
devleni <50588358+devleni@users.noreply.github.com>
2020-03-26 18:56:17 +0100

    add active cases to chart

--- src/CountryGraph.js
+++ src/CountryGraph.js
@@ -5,10 +5,42 @@ let CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
 class CountryGraph extends Component {
-  constructor() {
-		super();
+  constructor(props) {
+		super(props);
+		this.state = {
+      error: null,
+      isLoaded: false,
+      result: []
+    }; 
 		this.toggleDataSeries = this.toggleDataSeries.bind(this);
 	}
+
+	componentDidMount() {
+    fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "8ef55b2526mshfdc06b3fd66166dp16780ejsn5c0635d72882"
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            error: null,
+            isLoaded: true,
+            result: this.state.result.concat(result)
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
 	toggleDataSeries(e) {
 		if(typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
 			e.dataSeries.visible = false;
@@ -18,11 +50,46 @@ class CountryGraph extends Component {
 		}
 		this.chart.render();
 	}
+
 	render() {
+		const { error, isLoaded, result } = this.state;
+		const countryName1 = result.map(r => r.countries_stat[0].country_name);
+		const countryName2 = result.map(r => r.countries_stat[1].country_name);
+		const countryName3 = result.map(r => r.countries_stat[2].country_name);
+		const countryName4 = result.map(r => r.countries_stat[3].country_name);
+		const countryName5 = result.map(r => r.countries_stat[4].country_name);
+		const countryName6 = result.map(r => r.countries_stat[5].country_name);
+		const countryName7 = result.map(r => r.countries_stat[6].country_name);
+		const countryName8 = result.map(r => r.countries_stat[7].country_name);
+		const countryName9 = result.map(r => r.countries_stat[8].country_name);
+		const countryName10 = result.map(r => r.countries_stat[9].country_name);
+		const countryName11 = result.map(r => r.countries_stat[10].country_name);
+		const countryName12 = result.map(r => r.countries_stat[11].country_name);
+		const countryName13 = result.map(r => r.countries_stat[12].country_name);
+		const countryName14 = result.map(r => r.countries_stat[13].country_name);
+		const countryName15 = result.map(r => r.countries_stat[14].country_name);
+		const activeCases1 = parseInt(result.map(r => r.countries_stat[0].active_cases).toString().split(",").join(""));
+		const activeCases2 = parseInt(result.map(r => r.countries_stat[1].active_cases).toString().split(",").join(""));
+		const activeCases3 = parseInt(result.map(r => r.countries_stat[2].active_cases).toString().split(",").join(""));
+		const activeCases4 = parseInt(result.map(r => r.countries_stat[3].active_cases).toString().split(",").join(""));
+		const activeCases5 = parseInt(result.map(r => r.countries_stat[4].active_cases).toString().split(",").join(""));
+		const activeCases6 = parseInt(result.map(r => r.countries_stat[5].active_cases).toString().split(",").join(""));
+		const activeCases7 = parseInt(result.map(r => r.countries_stat[6].active_cases).toString().split(",").join(""));
+		const activeCases8 = parseInt(result.map(r => r.countries_stat[7].active_cases).toString().split(",").join(""));
+		const activeCases9 = parseInt(result.map(r => r.countries_stat[8].active_cases).toString().split(",").join(""));
+		const activeCases10 = parseInt(result.map(r => r.countries_stat[9].active_cases).toString().split(",").join(""));
+		const activeCases11 = parseInt(result.map(r => r.countries_stat[10].active_cases).toString().split(",").join(""));
+		const activeCases12 = parseInt(result.map(r => r.countries_stat[11].active_cases).toString().split(",").join(""));
+		const activeCases13 = parseInt(result.map(r => r.countries_stat[12].active_cases).toString().split(",").join(""));
+		const activeCases14 = parseInt(result.map(r => r.countries_stat[13].active_cases).toString().split(",").join(""));
+		const activeCases15 = parseInt(result.map(r => r.countries_stat[14].active_cases).toString().split(",").join(""));
+
+
 		const options = {
 			animationEnabled: true,
 			theme: "light2",			
 			axisX: {
+				interval: 1,
 				valueFormatString: "string"
 			},
 			axisY: {
@@ -39,23 +106,29 @@ class CountryGraph extends Component {
 				type: "stackedBar",
 				name: "Active Cases",
 				showInLegend: "true",
-				
 				yValueFormatString: "#",
 				dataPoints: [
-					{ label: "France", y: 20002, color: "crimson" },
-					{ label: "Iran", y: 16715, color: "crimson" },
-					{ label: "Germany", y: 35733, color: "crimson" },
-					{ label: "Spain", y: 45084, color: "crimson" },
-					{ label: "USA", y: 67309, color: "crimson" },
-					{ label: "Italy", y: 57521, color: "crimson" },
-					{ label: "China", y: 3947, color: "crimson" }
+					{ label: countryName15, y: activeCases15, color: "crimson" },
+					{ label: countryName14, y: activeCases14, color: "crimson" },
+					{ label: countryName13, y: activeCases13, color: "crimson" },
+					{ label: countryName12, y: activeCases12, color: "crimson" },
+					{ label: countryName11, y: activeCases11, color: "crimson" },
+					{ label: countryName10, y: activeCases10, color: "crimson" },
+					{ label: countryName9, y: activeCases9, color: "crimson" },
+					{ label: countryName8, y: activeCases8, color: "crimson" },
+					{ label: countryName7, y: activeCases7, color: "crimson" },
+					{ label: countryName6, y: activeCases6, color: "crimson" },
+					{ label: countryName5, y: activeCases5, color: "crimson" },
+					{ label: countryName4, y: activeCases4, color: "crimson" },
+					{ label: countryName3, y: activeCases3, color: "crimson" },
+					{ label: countryName2, y: activeCases2, color: "crimson" },
+					{ label: countryName1, y: activeCases1, color: "crimson" }
 				]
 			},
 			{
 				type: "stackedBar",
 				name: "Recovered",
 				showInLegend: "true",
-				
 				yValueFormatString: "#",
 				dataPoints: [
 					{ label: "France", y: 3900 },
@@ -71,7 +144,6 @@ class CountryGraph extends Component {
 				type: "stackedBar",
 				name: "Deaths",
 				showInLegend: "true",
-				
 				yValueFormatString: "#",
 				dataPoints: [
 					{ label: "France", y: 1331, color: "#333333" },
